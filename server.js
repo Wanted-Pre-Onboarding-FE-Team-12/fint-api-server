0ef5c1098bc2f2a885c990aec363987b427c622b
KiHoon Roh <started677@gmail.com>
2022-09-20 17:13:25 +0900

    fix: api 배포 오류 수정
    Middleware 적용

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const data = require('./db.js');
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
+const { wakeDyno } = require('heroku-keep-awake');
 
 const server = jsonServer.create();
 const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
 
 server.db = router.db;
 const rules = auth.rewriter({
@@ -12,6 +14,13 @@ const rules = auth.rewriter({
   accounts: 660,
 });
 
+const DYNO_URL = 'https://fint-api.herokuapp.com/';
+const opts = {
+  interval: 29,
+  logging: false,
+};
+
+server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(rules);
 server.use(auth);
@@ -19,4 +28,5 @@ server.use(router);
 
 server.listen(4000, () => {
   console.log('Server is Running');
+  wakeDyno(DYNO_URL, opts);
 });
